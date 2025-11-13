1f8842cb581505e5e01cb112ad5089cf1fdd74a5
Josh <joshuacitrin@gmail.com>
2025-11-13 01:53:43 -0800

    preparing data to be fed to classifier

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+import numpy as np
+from skimage.io import imread
+from skimage.transform import resize
+
+
+
+dir = './clf-data'
+cat = ['empty', 'not_empty']
+
+data = []
+labels = []
+
+for i, c in enumerate(cat):
+    for file in os.listdir(dir, c):
+        img_path = os.path.join(dir, c, file)
+        img = imread(img_path)
+        img = resize(img, (15, 15))
+        data.append(img.flatten())
+        labels.append(i)
+
+data = np.array(data)
+labels = np.array(labels)
